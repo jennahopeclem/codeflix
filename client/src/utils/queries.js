5c6c22f3e3fc952bd86812b9d80d2e603b51e7a0
wdiste <Wdiste49@gmail.com>
2023-02-10 14:27:38 -0500

    copied boilerplate and basic structure based on 21-MERN\28-mini-project.  We are ready to work

---
+++ client/src/utils/queries.js
@@ -0,0 +1,33 @@
+
+// import { gql } from '@apollo/client';
+
+
+// Exports to be used with useQuery() in components/pages
+
+// REFERENCE: 21-MERN\01-Activities\28-Stu_Mini-Project\Develop\client\src\utils\queries.js
+// ===================================================================
+// =============  BEGIN REFERENCE  ===========================================
+
+// export const QUERY_TECH = gql`
+//   query tech {
+//     tech {
+//       _id
+//       name
+//     }
+//   }
+// `;
+
+// export const QUERY_MATCHUPS = gql`
+//   query matchups($_id: String) {
+//     matchups(_id: $_id) {
+//       _id
+//       tech1
+//       tech2
+//       tech1_votes
+//       tech2_votes
+//     }
+//   }
+// `;
+
+// ===================================================================
+// =============  END REFERENCE  ===========================================
