5741384ab9b9358ef00e10cee1cc75bc777e1f44
jennahopeclem <jennahopeclem@gmail.com>
2023-02-13 14:57:09 -0500

    added in carousel/ fixing formatting

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
 
 // following code from 21-MERN\01-Activities\28-Stu_Mini-Project\Main\client\src\index.js
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,24 +1,24 @@
 import "./index.css";
 import Carousel from "../../components/displays/carousel";
-import { useQuery } from "@apollo/client";
-import { QUERY_PROJECT, QUERY_DEV, QUERY_COMPLETE } from "../utils/queries";
+// import { useQuery } from "@apollo/client";
+// import { QUERY_PROJECT, QUERY_DEV, QUERY_COMPLETE } from "../../utils/queries";
 
 function Home() {
   // const { loading, data } = useQuery(QUERY_PROJECT, {
   //   fetchPolicy: "no-cache",
   // });
 
-  const project = useQuery(QUERY_PROJECT, {
-    fetchPolicy: "no-cache",
-  });
+  // const project = useQuery(QUERY_PROJECT, {
+  //   fetchPolicy: "no-cache",
+  // });
 
-  const dev = useQuery(QUERY_DEV, {
-    fetchPolicy: "no-cache",
-  });
+  // const dev = useQuery(QUERY_DEV, {
+  //   fetchPolicy: "no-cache",
+  // });
 
-  const complete = useQuery(QUERY_COMPLETE, {
-    fetchPolicy: "no-cache",
-  });
+  // const complete = useQuery(QUERY_COMPLETE, {
+  //   fetchPolicy: "no-cache",
+  // });
 
   return (
     <>
@@ -30,10 +30,10 @@ function Home() {
           <h3>Get your project started</h3>
           {/* {loading ? (
             <div>Loading...</div> */}
-          
-          <Carousel group={project} />
+          <Carousel />
+          {/* <Carousel group={project} />
           <Carousel group={dev} />
-          <Carousel group={complete} />
+          <Carousel group={complete} /> */}
         </div>
       </div>
     </>
