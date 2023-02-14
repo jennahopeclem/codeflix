41dc51e2c96b5bd0604810f63c0b5cb8ecafe920
Kendrameda <gibsoken@gmail.com>
2023-02-14 09:17:34 -0600

    made login page functional

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -129,20 +129,24 @@ function Login() {
           
           <div class="mb-6">
             <input
-              type="text"
+            value={email}
+              name="email"
               class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
               id="exampleFormControlInput2"
               placeholder="Email address"
+              onChange={handleInputChange}
             />
           </div>
 
           
           <div class="mb-6">
             <input
+            value={password}
               type="password"
               class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
               id="exampleFormControlInput2"
               placeholder="Password"
+              onChange={handleInputChange}
             />
           </div>
 
@@ -161,7 +165,7 @@ function Login() {
           <div class="text-center lg:text-left">
             <button
               type="button"
-              class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+              class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={handleFormSubmit}>
               Login
             </button>
             <p class="text-sm font-semibold mt-2 pt-1 mb-0">
@@ -173,6 +177,11 @@ function Login() {
             </p>
           </div>
         </form>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
       </div>
     </div>
   </div>
